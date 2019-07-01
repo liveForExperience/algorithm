6ac0afa2b6f0c9c1117537e6a9273fbe0778a368
ChenYue <liveforexperience@126.com>
2019-07-01 13:32:34 +0800

    新增题53最大子序和1解，更新NOTE内容

--- Week_04/id_18/LeetCode_53_18.java
+++ Week_04/id_18/LeetCode_53_18.java
@@ -30,4 +30,18 @@ public class LeetCode_53_18 {
 
         return result;
     }
+
+    public int maxSubArray2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int result = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
 }
